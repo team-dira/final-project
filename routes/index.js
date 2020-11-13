937e323a9b2e05ae23fda2e1f694917ebbadd47d
dirabbieto <dirabbieto@gmail.com>
2020-11-13 12:56:30 +0700

    add user testing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-const UserController = require('../controllers/userController')
+// const router = require('express').Router()
+// const UserController = require('../controllers/userController')
 
-router.post('/users', UserController.register)
+// router.post('/users', UserController.register)
 
-module.exports = router
+// module.exports = router
