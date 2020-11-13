68eec9df64b123fa2fa219f92adae68f850ad70a
dirabbieto <dirabbieto@gmail.com>
2020-11-13 11:05:30 +0700

    change database to test

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const indexRoutes = require('./routes/index')
+const PORT = 3000
+
+app.use(express.urlencoded({ extended : false }))
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use('/', indexRoutes)
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`connected to port: ${PORT}`);
+})
+
+module.exports = app
